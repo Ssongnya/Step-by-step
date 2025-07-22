97a056ac20460ea54b0f8ba821e1524aa2d5a3dd
Ssongnya <alsrud9972@naver.com>
2025-07-22 15:32:24 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 3. 자료구조 - 셋, 딕셔너리 3, Time: 60 ms, Memory: 49,664 KB -BaekjoonHub

---
+++ SWEA/D1/6255. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 3． 자료구조 － 셋， 딕셔너리 3/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 3． 자료구조 － 셋， 딕셔너리 3.py
@@ -0,0 +1,6 @@
+funi = {"TV": 2000000, "냉장고": 1500000, "책상": 350000, "노트북": 1200000, "가스레인지": 200000, "세탁기": 1000000}
+
+sorted_funi = dict(sorted(funi.items(), key = lambda x: x[1], reverse=True))
+
+for item, price in sorted_funi.items() :
+    print(f'{item}: {price}')
