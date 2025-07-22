fc938e41dab41e190df05247de38519fdf2d69b1
Ssongnya <alsrud9972@naver.com>
2025-07-22 14:45:04 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 3. 자료구조 - 셋, 딕셔너리 2, Time: 55 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D1/6254. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 3． 자료구조 － 셋， 딕셔너리 2/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 3． 자료구조 － 셋， 딕셔너리 2.py
@@ -0,0 +1,16 @@
+phonebook = {'홍길동': '010-1111-1111', '이순신': '010-1111-2222', '강감찬': '010-1111-3333'}
+
+
+#program
+
+print("아래 학생들의 전화번호를 조회할 수 있습니다.")
+
+for key in phonebook :
+    print(key)
+
+
+print("전화번호를 조회하고자 하는 학생의 이름을 입력하십시오.")
+
+name = input()
+
+print(f'{name}의 전화번호는 {phonebook[name]}입니다.')
