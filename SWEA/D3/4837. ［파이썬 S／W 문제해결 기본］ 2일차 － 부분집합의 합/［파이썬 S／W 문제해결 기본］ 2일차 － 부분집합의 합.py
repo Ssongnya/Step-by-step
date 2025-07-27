b2fce88254132cec31e16990dcbce03a8fc3d5fd
Ssongnya <alsrud9972@naver.com>
2025-07-28 01:13:01 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 2일차 - 부분집합의 합, Time: 81 ms, Memory: 59,904 KB -BaekjoonHub

--- SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
+++ SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
@@ -1,30 +1,19 @@
 T = int(input())
-# A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-# 부분집합 함수 만들어 보기
-
-def subset(num, count, hap) : #부분집합에 넣을 숫자, 갯수, 합
-
-    global total
-
-    if count > N or hap > K :
-        return 0
-    
-    if num > 12 :   # 1부터 12까지 선택되는지 안되는지 다 확인한 후에,
-        if count == N and hap == K :    # 갯수와 합이 N, K와 동일하면
-            total += 1     # 부분집합 갯수 +1
-        return
-    
-    subset(num + 1, count + 1, hap + num)     # num 을 선택해서 count를 1 올려준다.
-
-    subset(num + 1, count, hap)   # num을 선택하지 않으면 count, hap 패스
-
-
 
 for t in range(1, T + 1) :
     N, K = map(int, input().split())
+    total = 0
 
-    total = 0   # 초기화 되면 안되고 누적 되어야 함
+    for i in range(1<<12) : # 총 원소의 갯수 만큼
+        hap = 0
+        count = 0
 
-    subset(1, 0, 0) # 1부터 포함되는지 안되는지 확인
-    print(f"#{t} {total}")
+        for k in range(12) :
+            if i & (1 << k) : # i에서 k번째 비트가 1인지 검사
+                hap += (k + 1)
+                count += 1
+        
+        if count == N and hap == K :
+            total += 1
+    
+    print(f'#{t} {total}')
