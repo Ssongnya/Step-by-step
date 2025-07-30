3ff6b865fd702221dd5cf801040509bbff48d178
Ssongnya <alsrud9972@naver.com>
2025-07-30 13:56:32 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 문자열 비교, Time: 59 ms, Memory: 51,328 KB -BaekjoonHub

--- SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
+++ SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
@@ -4,20 +4,9 @@ for t in range(1, T + 1) :
     N = input()
     M = input()
 
-    n = len(N)
-    m = len(M)
-
-    result = 0
-
-    for i in range(m - n + 1) :
-        if M[i:i+n] == N :
-            result += 1
-            break
-        
-        else :
-            continue
-
-    if result == 1 :
-        print(f'#{t}', 1)
+    if N in M :
+        result = 1
     else :
-        print(f'#{t}', 0)
+        result = 0
+
+    print(f'#{t} {result}')
