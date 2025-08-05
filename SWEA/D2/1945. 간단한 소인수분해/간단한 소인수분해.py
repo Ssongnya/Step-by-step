2d54d7fac326b8d7bf0aff1fc8f96c2904bf5a51
Ssongnya <alsrud9972@naver.com>
2025-08-05 15:16:28 +0900

    [D2] Title: 간단한 소인수분해, Time: 47 ms, Memory: 45,824 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,14 +1,26 @@
 T = int(input())
-
+ 
 for t in range(1, T + 1):
     N = int(input())
     count = [0] * 5
-    prime_lst = [2, 3, 5, 7, 11]
-
-    for idx, prime in enumerate(prime_lst):
-        while N % prime == 0:
-            N /= prime
-            count[idx] += 1
-
-    print(f"#{t}", *count)
-
+    while N % 2 == 0:
+        N /= 2
+        count[0] += 1
+ 
+    while N % 3 == 0:
+        N /= 3
+        count[1] += 1
+ 
+    while N % 5 == 0:
+        N /= 5
+        count[2] += 1
+ 
+    while N % 7 == 0:
+        N /= 7
+        count[3] += 1
+ 
+    while N % 11 == 0:
+        N /= 11
+        count[4] += 1
+ 
+    print(f"#{t} {' '.join(map(str, count))}")
