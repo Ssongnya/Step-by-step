a3b441caa2030d55c3b1f156080a61a67d071d46
Ssongnya <alsrud9972@naver.com>
2025-08-05 16:44:40 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 4일차 - 그래프 경로, Time: 60 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/4871. ［파이썬 S／W 문제해결 기본］ 4일차 － 그래프 경로/［파이썬 S／W 문제해결 기본］ 4일차 － 그래프 경로.py
+++ SWEA/D2/4871. ［파이썬 S／W 문제해결 기본］ 4일차 － 그래프 경로/［파이썬 S／W 문제해결 기본］ 4일차 － 그래프 경로.py
@@ -1,42 +1,30 @@
-T = int(input())
 
-
-        
-
-def dfs_stack(graph, start):
+def find_direct(graph, start):
     visited = []
     stack = [start]
 
-    # 스택이 빌 때까지 반복
     while stack:
-        node = stack.pop()  # 스택의 가장 위(마지막) 노드를 꺼냄
-        if node not in visited:  # 아직 방문하지 않았다면
-            visited.append(node)  # 방문 리스트에 추가
-            # 현재 노드와 연결된 인접 노드를 확인
-            # reversed를 쓰는 이유: 스택은 LIFO라서 역순으로 넣어야 작은 번호부터 방문 가능
-            for neighbor in reversed(graph[node]):
-                if neighbor not in visited:
-                    stack.append(neighbor)  # 방문하지 않은 노드를 스택에 추가
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            for near in graph[node]:
+                if near not in visited:
+                    stack.append(near)
     return visited
 
-for t in range(1, T+1) :
+
+T = int(input())
+
+for t in range(1, T + 1):
     V, E = map(int, input().split())
-    direct = []
-    for i in range(E):
-        direct.append(list(map(int, input().split())))
-    
+    direct = {x: [] for x in range(1, V+1)}
+    for _ in range(E):
+        d, e = map(int, input().split())
+        direct[d].append(e)
+
     S, G = map(int, input().split())
-    
-    graph = {k : [] for k in range(1, V+1)}
 
-    for e in direct :
-        a, b = e
-        graph[a].append(b)
-        # graph[b].append(a) -> 간선이기 때문에 한 방향으로만 진행해야함!
-    
-    
-
-    if G in dfs_stack(graph, S):
-        print(f'#{t}', 1)
-    else :
-        print(f'#{t}', 0)
+    if G in find_direct(direct, S):
+        print(f"#{t} 1")
+    else:
+        print(f"#{t} 0")
