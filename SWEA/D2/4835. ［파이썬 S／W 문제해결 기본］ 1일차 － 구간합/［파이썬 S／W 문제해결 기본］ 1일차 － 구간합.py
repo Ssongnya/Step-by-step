6c02c2e04059517b576128de6d9fb0a710915bc1
Ssongnya <alsrud9972@naver.com>
2025-08-05 15:28:33 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 구간합, Time: 58 ms, Memory: 52,736 KB -BaekjoonHub

--- SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
+++ SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
@@ -1,19 +1,19 @@
 T = int(input())
 
-for t in range(1, T+1):
+for t in range(1, T + 1):
     N, M = map(int, input().split())
     ai = list(map(int, input().split()))
     sum_lst = []
     for i in range(N - M + 1):
         hap = 0
-        for k in range(i, i+M):
+        for k in range(i, i + M):
             hap += ai[k]
         sum_lst.append(hap)
 
     max_sum = sum_lst[0]
     min_sum = sum_lst[0]
 
-    for num in sum_lst :
+    for num in sum_lst:
         if num > max_sum:
             max_sum = num
         if num < min_sum:
