88933bb9cd7857f1441b95ae81cac641e7493b6a
Ssongnya <alsrud9972@naver.com>
2025-08-05 13:37:45 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 숫자 카드, Time: 58 ms, Memory: 52,736 KB -BaekjoonHub

--- SWEA/D2/4834. ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
+++ SWEA/D2/4834. ［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드/［파이썬 S／W 문제해결 기본］ 1일차 － 숫자 카드.py
@@ -1,33 +1,27 @@
 T = int(input())
 
-# for i in range(1, T + 1) :
-#     N = int(input())
-#     a_i = input()   # 인덱싱을 위해 문자열로
-#     many = {}
-#     for num in a_i :
-#         if num not in many.keys() :
-#             many[num] = 1
-#         else :
-#             many[num] += 1
-    
-#     print(f'#{i} {max(many)} {many[max(many)]}')
-# ----> 오류발생 문자열로해서 생긴 오류인듯
-
-for i in range(1, T + 1) :
+for t in range(1, T+1):
     N = int(input())
-    a_i = input()
-
-    if len(a_i) != N :
-        break   # 인덱싱을 위해 문자열로
-    
-    many = {}
-
-    for num in a_i :
-        if num in many.keys() :
-            many[num] += 1
+    nums = input()
+    nums_dict = {}
+    for i in nums:
+        if i in nums_dict.keys():
+            nums_dict[i] += 1
         else :
-            many[num] = 1
+            nums_dict[i] = 1
 
-    max_key, max_count = max(many.items(), key = lambda x : (x[1], int(x[0])))
+    key_lst = list(nums_dict.keys())
+    value_lst = list(nums_dict.values())
 
-    print(f'#{i} {max_key} {max_count}')
+    max_idx = 0
+    max_value = value_lst[0]
+
+    for j in range(len(value_lst)):
+        if value_lst[j] > max_value:
+            max_value = value_lst[j]
+            max_idx = j
+        elif value_lst[j] == max_value :
+            if int(key_lst[j]) > int(key_lst[max_idx]):
+                max_idx = j
+
+    print(f"#{t} {int(key_lst[max_idx])} {max_value}")
