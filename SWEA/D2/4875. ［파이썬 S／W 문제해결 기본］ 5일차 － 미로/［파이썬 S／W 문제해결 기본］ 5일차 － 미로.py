d4d170c75d07098a0beb3c3b950a6932d6cc8ecf
Ssongnya <alsrud9972@naver.com>
2025-08-06 23:34:56 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 미로, Time: 97 ms, Memory: 67,328 KB -BaekjoonHub

--- SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
+++ SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
@@ -2,45 +2,33 @@ T = int(input())
 
 for t in range(1, T + 1):
     N = int(input())
-
     maze = [[int(x) for x in input().strip()] for _ in range(N)]
 
-    visited = [[0]*N for _ in range(N)]
+    start_pos = None
+    end_pos = None
+    for i in range(N):
+        for j in range(N):
+            if maze[i][j] == 2:
+                start_pos = (i, j)
+            elif maze[i][j] == 3:
+                end_pos = (i, j)
+
     near = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-    def is_Valid(x, y):
-        return 0 <= x < N and 0 <= y < N and maze[x][y] != 1 and not visited[x][y]
-    
-    def start(arr):
-        for i in range(N):
-            for j in range(N):
-                if arr[i][j] == 2:
-                    return (i, j)
-    
-
-    def end(arr):
-        for i in range(N):
-            for j in range(N):
-                if arr[i][j] == 3:
-                    return (i, j)
-        
     def backtrack(x, y):
-        if (x, y) == end(maze):
+        if (x, y) == end_pos:  # 도착점 도달
             return True
-        
-        visited[x][y] = 1
+
+        maze[x][y] = 1  # 방문 표시
         for dx, dy in near:
             nx, ny = x + dx, y + dy
-            if is_Valid(nx, ny):
+            if 0 <= nx < N and 0 <= ny < N and maze[nx][ny] != 1:
                 if backtrack(nx, ny):
                     return True
-        visited[x][y] = 0
-        return 0
-    
-    start_place = start(maze)
+        maze[x][y] = 0  # 되돌리기
+        return False
 
-    if backtrack(*start_place):
-        print(f"#{t}", 1)
+    if backtrack(*start_pos):
+        print(f"#{t} 1")
     else:
-        print(f"#{t}", 0)
-
+        print(f"#{t} 0")
