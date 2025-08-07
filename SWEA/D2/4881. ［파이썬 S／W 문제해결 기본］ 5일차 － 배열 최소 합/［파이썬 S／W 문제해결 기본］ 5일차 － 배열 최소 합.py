698a943a7523a6ca1fb9c362f2d25ffc436b57c0
Ssongnya <alsrud9972@naver.com>
2025-08-07 11:04:55 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 배열 최소 합, Time: 100 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
+++ SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
@@ -1,30 +1,30 @@
-T = int(input())
-
-def dfs(row, current_sum):
+def find_sum(x, now_sum):
     global min_sum
 
-    # 가지치기: 현재 합이 이미 최소 합보다 크면 중단
-    if current_sum >= min_sum:
+    if now_sum >= min_sum:
         return
-
-    # 모든 행을 다 선택했으면 최소 합 갱신
-    if row == n:
-        min_sum = current_sum
+    
+    if x == N :
+        min_sum = now_sum
         return
+    
+    for y in range(N):
+        if not visited[y]:
+            visited[y] = True
+            find_sum(x + 1, now_sum + nums[x][y])
+            visited[y] = False
 
-    # 현재 행에서 가능한 열 선택
-    for col in range(n):
-        if not used[col]:  # 아직 사용 안한 열이면 선택 가능
-            used[col] = True
-            dfs(row + 1, current_sum + cost[row][col])
-            used[col] = False  # 원상복구 (백트래킹)
 
-for tc in range(1, T + 1):
-    n = int(input())
-    cost = [list(map(int, input().split())) for _ in range(n)]
 
-    used = [False] * n  # 각 열 선택 여부
-    min_sum = float('inf')
+T = int(input())
 
-    dfs(0, 0)
-    print(f"#{tc} {min_sum}")
+for t in range(1, T + 1):
+    N = int(input())
+    nums = [list(map(int, input().split())) for _ in range(N)]
+    visited = [False] * N
+
+    min_sum = 10**10 + 1
+    
+    find_sum(0, 0)
+
+    print(f"#{t} {min_sum}")
