eb22f71abc4a071f4b85c81a3f7498ec82f95a95
Ssongnya <alsrud9972@naver.com>
2025-08-07 12:21:21 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 2일차 - 이진탐색, Time: 58 ms, Memory: 51,072 KB -BaekjoonHub

--- SWEA/D2/4839. ［파이썬 S／W 문제해결 기본］ 2일차 － 이진탐색/［파이썬 S／W 문제해결 기본］ 2일차 － 이진탐색.py
+++ SWEA/D2/4839. ［파이썬 S／W 문제해결 기본］ 2일차 － 이진탐색/［파이썬 S／W 문제해결 기본］ 2일차 － 이진탐색.py
@@ -1,40 +1,37 @@
 T = int(input())
 
-# 이진 탐색 함수 만들기
 
-def binary(page, want) :
-    l = 1
-    r = page
+def binary(page, wanted_page):
+
+    left = 1
+    right = page
+
     count = 0
 
-    while l <= r :
-        
+    while left <= right:
+        mid = (left + right) // 2
         count += 1
 
-        c = l + (r - l) // 2
-
-        if want == c :
+        if wanted_page == mid:
             return count
-        elif want < c :
-            r = c
-        else :
-            l = c
+        elif wanted_page > mid:
+            left = mid
+        else:
+            right = mid
 
     return count
-    
 
 
-for t in range(1, T + 1) :
+for t in range(1, T + 1):
     P, Pa, Pb = map(int, input().split())
-    
-    count_a = binary(P, Pa)
-    count_b = binary(P, Pb)
-    
-    if count_a > count_b :
-        winner = 'B'
-    elif count_a == count_b :
-        winner = 0
-    else :
-        winner = 'A'
+    count_A = binary(P, Pa)
+    count_B = binary(P, Pb)
 
-    print(f'#{t} {winner}')
+    if count_A < count_B:
+        result = "A"
+    elif count_A == count_B:
+        result = 0
+    else:
+        result = "B"
+
+    print(f"#{t} {result}")
