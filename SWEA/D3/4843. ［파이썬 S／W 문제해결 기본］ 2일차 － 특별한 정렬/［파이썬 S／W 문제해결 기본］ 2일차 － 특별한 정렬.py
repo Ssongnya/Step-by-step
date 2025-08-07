79880b69e98f165e42d7074a7a5cc864ef1da5c9
Ssongnya <alsrud9972@naver.com>
2025-08-07 12:05:19 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 2일차 - 특별한 정렬, Time: 59 ms, Memory: 49,920 KB -BaekjoonHub

--- SWEA/D3/4843. ［파이썬 S／W 문제해결 기본］ 2일차 － 특별한 정렬/［파이썬 S／W 문제해결 기본］ 2일차 － 특별한 정렬.py
+++ SWEA/D3/4843. ［파이썬 S／W 문제해결 기본］ 2일차 － 특별한 정렬/［파이썬 S／W 문제해결 기본］ 2일차 － 특별한 정렬.py
@@ -1,22 +1,38 @@
 T = int(input())
 
-for t in range(1, T + 1) :
+
+def find_max(lst):
+    global nums
+    max_num = -1
+    max_idx = -1
+    for n in range(len(lst)):
+        if lst[n] > max_num:
+            max_num = lst[n]
+            max_idx = n
+    nums.pop(max_idx)
+    return max_num
+
+
+def find_min(lst):
+    global nums
+    min_num = 100000000
+    min_idx = -1
+    for n in range(len(lst)):
+        if lst[n] < min_num:
+            min_num = lst[n]
+            min_idx = n
+    nums.pop(min_idx)
+    return min_num
+
+
+for t in range(1, T + 1):
     N = int(input())
     nums = list(map(int, input().split()))
 
-    nums.sort()
-    result = []
-
-    left = 0
-    right = N - 1
-
-    for i in range(10) :
-        if i % 2 == 0 :
-            result.append(nums[right])
-            right -= 1
-
-        else :
-            result.append(nums[left])
-            left += 1
-
-    print(f'#{t}', *result)
+    print(f"#{t}", end=" ")
+    for i in range(10):
+        if i % 2 == 0:
+            print(find_max(nums), end=" ")
+        else:
+            print(find_min(nums), end=" ")
+    print("")
