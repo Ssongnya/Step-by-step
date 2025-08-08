67c300319c01aa6152531afd470e255a9f71e44a
Ssongnya <alsrud9972@naver.com>
2025-08-08 11:29:49 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 문자열 비교, Time: 63 ms, Memory: 49,792 KB -BaekjoonHub

--- SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
+++ SWEA/D2/4864. ［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교/［파이썬 S／W 문제해결 기본］ 3일차 － 문자열 비교.py
@@ -1,12 +1,28 @@
+#고지식하게 풀어보기
+
 T = int(input())
 
-for t in range(1, T + 1) :
-    N = input()
-    M = input()
 
-    if N in M :
-        result = 1
-    else :
-        result = 0
+def brute_force(txt, pattern):
+    x, y = len(txt), len(pattern)
 
-    print(f'#{t} {result}')
+    for i in range(x - y + 1):
+        match = True
+        for j in range(y):
+            if txt[i + j] != pattern[j]:
+                match = False
+                break
+        if match:
+            return 1
+
+    return 0
+
+
+for t in range(1, T + 1):
+
+    str1 = input()
+    str2 = input()
+
+    result = brute_force(str2, str1)
+
+    print(f"#{t} {result}")
