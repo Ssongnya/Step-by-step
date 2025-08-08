322c790c318715cf24f34380cd0d9c4138c4f7a9
Ssongnya <alsrud9972@naver.com>
2025-08-08 10:13:32 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 회문, Time: 88 ms, Memory: 59,776 KB -BaekjoonHub

--- SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
+++ SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
@@ -1,29 +1,29 @@
 T = int(input())
 
-for t in range(1, T + 1) :
+for t in range(1, T + 1):
     N, M = map(int, input().split())
-    result = []
 
-    # 배열 입력 받기
-    grid = []
-    for num in range(N):
-        grid.append(list(input().strip())) # 리스트로 저장
-    
-    # 가로 검색
-    for row in range(N):
-        for column in range(N - M + 1) :
-            find_row = grid[row][column:column + M]
-            if find_row == find_row[::-1] :
-                result.append("".join(find_row)) # 문자열 반환
-    
-    # 세로 검색
+    words_board = [list(input().strip()) for _ in range(N)]
 
-    for column in range(N): # 열 고정 후 같은 열 끼리 비교
-        for row in range(N - M + 1) : # 시작 행
-            col_txt = []
-            for find_col in range(M) : # M 길이만큼 문자 추출
-                col_txt.append(grid[row + find_col][column])
-            if col_txt == col_txt[::-1] :
-                result.append("".join(col_txt))
-    
-    print(f'#{t} {result[0]}')
+    found = False
+
+    for i in range(N):
+        for j in range(N - M + 1):
+            find_word = words_board[i][j:j+M]
+            if find_word == find_word[::-1]:
+                found = "".join(find_word)
+                break
+        if found:
+            break
+
+    if not found:
+        for i in range(N - M + 1):
+            for j in range(N):
+                find_word = [words_board[i+k][j] for k in range(M)]
+                if find_word == find_word[::-1]:
+                    found = "".join(find_word)
+                    break
+            if found:
+                break
+
+    print(f"#{t} {found}")
