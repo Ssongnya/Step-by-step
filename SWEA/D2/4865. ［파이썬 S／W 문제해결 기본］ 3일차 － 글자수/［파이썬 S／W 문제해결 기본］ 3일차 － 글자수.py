c0ca80ccaac72fd30e3dbffcb16239118bd661e3
Ssongnya <alsrud9972@naver.com>
2025-08-08 09:39:13 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 글자수, Time: 60 ms, Memory: 52,224 KB -BaekjoonHub

--- SWEA/D2/4865. ［파이썬 S／W 문제해결 기본］ 3일차 － 글자수/［파이썬 S／W 문제해결 기본］ 3일차 － 글자수.py
+++ SWEA/D2/4865. ［파이썬 S／W 문제해결 기본］ 3일차 － 글자수/［파이썬 S／W 문제해결 기본］ 3일차 － 글자수.py
@@ -1,14 +1,23 @@
 T = int(input())
 
-for t in range(1, T+1) :
-    str1 = input()
-    str2 = input()
 
-    count = {}
+def text_count(txt):
+    txt_dict = {}
+    for i in txt:
+        txt_dict[i] = txt_dict.get(i, 0) + 1
+    return txt_dict
 
-    for txt in str1:
-        count[txt] = str2.count(txt)
-    
-        max_count = max(count.values())
-    
-    print(f'#{t} {max_count}')
+
+for t in range(1, T + 1):
+    str1 = text_count(input().strip())
+    str2 = text_count(input().strip())
+
+    same_dict = {x: str2[x] for x in str1.keys()}
+
+    max_value = -1
+
+    for j in same_dict:
+        if same_dict[j] > max_value:
+            max_value = same_dict[j]
+
+    print(f"#{t} {max_value}")
