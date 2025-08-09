cc81f475b4a00a5d55dda3002fc3ed706f848e54
Ssongnya <alsrud9972@naver.com>
2025-08-09 23:49:55 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 80 ms, Memory: 53,888 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,35 +1,34 @@
 T = int(input())
 
-for t in range(1, T+1):
+for t in range(1, T + 1):
     N, K = map(int, input().split())
-    grid = [[int(x) for x in input().split()] for _ in range(N)]
+    grid = [list(map(int, input().split())) for _ in range(N)]
 
-    hap = []
+    total = 0
 
     for i in range(N):
         count = 0
         for j in range(N):
-            if grid[i][j] == 1:
+            if grid[i][j] == 1 :
                 count += 1
             else :
-                hap.append(count)
+                if count == K:
+                    total += 1
                 count = 0
-        hap.append(count)
-
+        if count == K :
+            total += 1
+        
     for j in range(N):
         count = 0
         for i in range(N):
-            if grid[i][j] == 1:
+            if grid[i][j] == 1 :
                 count += 1
-            else:
-                hap.append(count)
+            else :
+                if count == K:
+                    total += 1
                 count = 0
-        hap.append(count)
+        if count == K :
+            total += 1
 
-    hap_count = 0
-
-    for n in hap:
-        if n == K:
-            hap_count += 1
-
-    print(f"#{t} {hap_count}")
+    print(f"#{t} {total}")
+    
