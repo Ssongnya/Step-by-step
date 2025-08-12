cb6b455dbc2242352cf0b65c148e2623bd2e5bda
Ssongnya <alsrud9972@naver.com>
2025-08-12 15:50:51 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 노드의 거리, Time: 62 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/5102. ［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리.py
+++ SWEA/D2/5102. ［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리.py
@@ -3,19 +3,19 @@ T = int(input())
 for t in range(1, T + 1):
     V, E = map(int, input().split())
 
-    near = [[] for _ in range(V+1)]
+    near = [[] for _ in range(V + 1)]
 
     for _ in range(E):
         a, b = map(int, input().split())
         near[a].append(b)
-        near[b].append(a)   # 양방향에 전부 간선 정보 입력
-    
+        near[b].append(a)
+
     S, G = map(int, input().split())
 
-    visited = [-1] * (V + 1)    # -1로 초기화 0번 인덱스는 안쓸거니까 V+1
+    visited = [-1] * (V + 1)
     queue = [S]
     front = 0
-    visited[S] = 0
+    visited[S] = 0  # 방문처리
 
     while front < len(queue):
         now = queue[front]
@@ -23,11 +23,11 @@ for t in range(1, T + 1):
 
         if now == G:
             break
-    
-        for j in near[now]:
-            if visited[j] == -1 :
-                visited[j] = visited[now] + 1
-                queue.append(j)
 
+        for i in near[now]:
+            if visited[i] == -1:
+                visited[i] = visited[now] + 1
+                queue.append(i)
+    
     result = visited[G] if visited[G] != -1 else 0
     print(f"#{t} {result}")
