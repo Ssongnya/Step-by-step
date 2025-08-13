403cc36640c11e06350358ba1b4f501d742a083b
Ssongnya <alsrud9972@naver.com>
2025-08-13 12:06:29 +0900

    [D1] Title: 돌 뒤집기 게임 2, Time: 63 ms, Memory: 51,072 KB -BaekjoonHub

--- SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.py
+++ SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.py
@@ -3,23 +3,22 @@ T = int(input())
 for t in range(1, T + 1):
     N, M = map(int, input().split())
     stones = list(map(int, input().split()))
-    for m in range(M):
+
+    for _ in range(M):
         i, j = map(int, input().split())
-        
+
         I = i - 1
 
-        for k in range(1, j + 1):
+        for k in range(1, j+1):
             left = I - k
             right = I + k
 
-            if left < 0 or right >= N :
+            if left < 0 and right >= N:
                 break
 
-            if stones[left] == stones[right]:
-                stones[left] = 1 - stones[left]
-                stones[right] = 1 - stones[right]
-    
+            if left >= 0 and right < N:
+                if stones[left] == stones[right]:
+                    stones[left] ^= 1
+                    stones[right] ^= 1
+
     print(f"#{t}", *stones)
-
-
-    
