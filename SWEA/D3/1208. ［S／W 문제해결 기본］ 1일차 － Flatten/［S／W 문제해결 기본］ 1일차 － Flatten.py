9803ae1b29f340f563c79125fffbf4229b8e11f9
Ssongnya <alsrud9972@naver.com>
2025-08-18 23:36:31 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 74 ms, Memory: 58,368 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,33 +1,12 @@
-def find_max(lst):
-    max_box = lst[0]
-    max_idx = 0
-    for idx in range(100):
-        if lst[idx] > max_box :
-            max_box = lst[idx]
-            max_idx = idx
-    return [max_box, max_idx]
-
-
-def find_min(lst):
-    min_box = lst[0]
-    min_idx = 0
-    for idx in range(100):
-        if lst[idx] < min_box:
-            min_box = lst[idx]
-            min_idx = idx
-    return [min_box, min_idx]
-
-def flatten(lst):
-    lst[find_max(lst)[1]] -= 1
-    lst[find_min(lst)[1]] += 1
-    return lst
-
-
 for t in range(1, 11):
     dump = int(input())
     boxes = list(map(int, input().split()))
 
-    for _ in range(dump):
-        flatten(boxes)
+    for d in range(dump):
+        max_idx = boxes.index(max(boxes))
+        min_idx = boxes.index(min(boxes))
 
-    print(f"#{t} {find_max(boxes)[0]- find_min(boxes)[0]}")
+        boxes[max_idx] -= 1
+        boxes[min_idx] += 1
+
+    print(f"#{t} {max(boxes)-min(boxes)}")
