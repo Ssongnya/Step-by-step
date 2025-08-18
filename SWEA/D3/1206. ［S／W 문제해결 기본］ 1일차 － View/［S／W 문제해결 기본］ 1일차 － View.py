7b6bc3796773a1ccc4d8a14488a20a0869de5c5a
Ssongnya <alsrud9972@naver.com>
2025-08-18 23:24:07 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 75 ms, Memory: 59,392 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,31 +1,15 @@
 for t in range(1, 11):
     N = int(input())
-    building = list(map(int, input().split()))
-    find = 0
 
-    for i in range(2, len(building) - 2):
-        left2 = building[i - 2]
-        left1 = building[i - 1]
-        right1 = building[i + 1]
-        right2 = building[i + 2]
+    buildings = list(map(int, input().split()))
+    sunlight = 0
 
-        if left2 > building[i] or left1 > building[i] or right2 > building[i] or right1 > building[i]:
-            continue
-        else:
-            diff1 = building[i] - left2
-            diff2 = building[i] - left1
-            diff3 = building[i] - right2
-            diff4 = building[i] - right1
+    for b in range(2, N-2):
+        max_diff = 0
+        brange = [buildings[b-2], buildings[b-1], buildings[b+1], buildings[b+2]]
+        if buildings[b] > max(brange):
+            sunlight += buildings[b] - max(brange)
 
-            min_diff = diff1
+    print(f"#{t} {sunlight}")
 
-            if diff2 < min_diff:
-                min_diff = diff2
-            if diff3 < min_diff:
-                min_diff = diff3
-            if diff4 < min_diff:
-                min_diff = diff4
 
-            find += min_diff
-
-    print(f'#{t} {find}')
