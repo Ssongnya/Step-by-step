52fd0d15c58ad04922f1d6c663ce7c1602cfa21a
Ssongnya <alsrud9972@naver.com>
2025-08-21 21:58:05 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 90 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,47 +1,39 @@
-
 from collections import deque
-
-
+ 
+ 
 def find_start(arr):
     n = len(arr)
     for i in range(n):
         for j in range(n):
             if arr[i][j] == 2:
                 return i, j
-def find_goal(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == 3:
-                return i, j
-
+ 
 for t in range(1, 11):
     T = int(input())
-    
+     
     maze = [list(map(int, input().strip())) for _ in range(16)]
     visited = [[0] * 16 for _ in range(16)]
     delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
+ 
     sx, sy = find_start(maze)
-    gx, gy = find_goal(maze)
-
+ 
     direct = deque([(sx, sy)])
     visited[sx][sy] = 1
     result = 0
-
+ 
     while direct:
         x, y = direct.popleft()
-
+ 
         for dx, dy in delta:
             nx, ny = x + dx, y + dy
-
+ 
             if 0 <= nx < 16 and 0 <= ny < 16 and maze[nx][ny] != 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = visited[x][y] + 1
-                if (nx, ny) == (gx, gy):
+                if maze[nx][ny] == 3:
                     result = 1
                     found = True
                     break
-
+ 
                 direct.append((nx, ny))
-
+ 
     print(f"#{t} {result}")
