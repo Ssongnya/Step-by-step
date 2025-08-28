9869c13db64cc1bb96e1e4a1bfa23e9ab7f6ad18
Ssongnya <alsrud9972@naver.com>
2025-08-28 17:40:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 2552 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+
+product = [tuple(map(int, input().split())) for _ in range(N)]
+
+weight = [0] * (K + 1)
+
+for w, v in product:
+    for i in range(K, w-1, -1): 
+        # weight[K-i] 
+        weight[i] = max(weight[i - w] + v, weight[i])
+
+print(weight[K])
