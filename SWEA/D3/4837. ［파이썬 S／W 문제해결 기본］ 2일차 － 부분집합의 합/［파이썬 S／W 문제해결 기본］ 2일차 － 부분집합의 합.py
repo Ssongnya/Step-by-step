b1dc5faa88cbd414d17fdfaa50b41b902d0b7bf7
Ssongnya <alsrud9972@naver.com>
2025-09-08 20:53:17 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 2일차 - 부분집합의 합, Time: 85 ms, Memory: 60,672 KB -BaekjoonHub

--- SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
+++ SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
@@ -1,19 +1,28 @@
 T = int(input())
 
-for t in range(1, T + 1) :
+
+def subset(num, cnt, hap):
+    global total
+
+    if cnt > N and hap > K:
+        return 0
+
+    if num > 12:
+        if cnt == N and hap == K:
+            total += 1
+        return
+    
+    subset(num + 1, cnt, hap)
+    subset(num + 1, cnt + 1, hap + num)
+    
+
+for t in range(1, T + 1):
     N, K = map(int, input().split())
+
     total = 0
 
-    for i in range(1<<12) : # 총 원소의 갯수 만큼
-        hap = 0
-        count = 0
-
-        for k in range(12) :
-            if i & (1 << k) : # i에서 k번째 비트가 1인지 검사
-                hap += (k + 1)
-                count += 1
-        
-        if count == N and hap == K :
-            total += 1
+    subset(1, 0, 0)
     
-    print(f'#{t} {total}')
+    print(f"#{t} {total}")
+
+    
