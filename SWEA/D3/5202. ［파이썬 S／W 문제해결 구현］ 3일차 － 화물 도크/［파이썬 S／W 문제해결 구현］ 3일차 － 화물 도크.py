f317daa7b9213cc71fa00dc1e211405085db781e
Ssongnya <alsrud9972@naver.com>
2025-09-08 15:17:10 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 3일차 - 화물 도크, Time: 67 ms, Memory: 53,888 KB -BaekjoonHub

---
+++ SWEA/D3/5202. ［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크/［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for t in range(1, T + 1):
+    N = int(input())
+
+    schedule = [tuple(map(int, input().split())) for _ in range(N)]
+
+    schedule.sort(key= lambda x : x[1])
+
+    time = [False for _ in range(24)]
+
+    cnt = 0
+    end = 0
+
+    for x, y in schedule:
+        if x >= end:
+            cnt += 1
+            end = y
+
+    print(f"#{t} {cnt}")
