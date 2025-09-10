5220c2af89f8a5061dd1d83c45c53c09d3f69e97
Ssongnya <alsrud9972@naver.com>
2025-09-10 15:33:43 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,36 +1,56 @@
-N = int(input())
+import sys
+alpha_dict = {chr(ord('A') + i): i for i in range(26)}
 
-tree = {}
 
-for _ in range(N):
-    key, c1, c2 = input().split()
-    tree[key] = [c1, c2]
-
-def preorder(node):
-    if node == ".":
+def pre_order(node: str):
+    if node == '.':
         return
-    print(node, end="")
-    preorder(tree[node][0])
-    preorder(tree[node][1])
+    print(node, end='')
+    pre_order(left_tree[alpha_dict[node]])
+    pre_order(right_tree[alpha_dict[node]])
 
-def inorder(node):
-    if node == ".":
+
+def in_order(node: str):
+    if node == '.':
         return
-    
-    inorder(tree[node][0])
-    print(node, end="")
-    inorder(tree[node][1])
+    in_order(left_tree[alpha_dict[node]])
+    print(node, end='')
+    in_order(right_tree[alpha_dict[node]])
 
-def postorder(node):
-    if node == ".":
+
+def post_order(node: str):
+    if node == '.':
         return
-    
-    postorder(tree[node][0])
-    postorder(tree[node][1])
-    print(node, end="")
+    post_order(left_tree[alpha_dict[node]])
+    post_order(right_tree[alpha_dict[node]])
+    print(node, end='')
 
-preorder("A")
+
+Node = int(sys.stdin.readline())
+
+left_tree = ['.'] * 26
+right_tree = ['.'] * 26
+parent = [0] * 26 
+
+for _ in range(Node):
+    root, left, right = sys.stdin.readline().split()
+    idx = alpha_dict[root]
+    left_tree[idx] = left
+    right_tree[idx] = right
+    if left != '.':
+        parent[alpha_dict[left]] = 1
+    if right != '.':
+        parent[alpha_dict[right]] = 1
+
+root_node = '.'
+for ch, i in alpha_dict.items():
+    if parent[i] == 0 and (left_tree[i] != '.' or right_tree[i] != '.' or ch == 'A'):
+        root_node = ch
+        break
+
+pre_order(root_node)
 print()
-inorder("A")
+in_order(root_node)
 print()
-postorder("A")
+post_order(root_node)
+print()
