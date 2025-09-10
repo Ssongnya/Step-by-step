9a290cd9819335071bdf3cf4d205a93b0604d574
Ssongnya <alsrud9972@naver.com>
2025-09-10 23:24:27 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1080 ms, Memory: 154864 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+N, M, K, X = map(int, input().split())
+
+road = [[] for _ in range(N + 1)]
+visited = [0 for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    road[a].append(b)
+
+visited[X] = 1
+queue = deque([X])
+
+while queue:
+    now = queue.popleft()
+    
+    for next in road[now]:
+        if visited[next] == 0:
+            visited[next] = visited[now] + 1
+            queue.append(next)
+
+found = False
+for i in range(1, N + 1):
+    if visited[i] - 1 == K:
+        found = True
+        print(i)
+
+if not found:
+    print(-1)
