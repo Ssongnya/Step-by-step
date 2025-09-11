fb4525ca69107d3cb3057801058c4e5b17de730d
Ssongnya <alsrud9972@naver.com>
2025-09-11 17:05:25 +0900

    [D4] Title: 정사각형 방, Time: 138 ms, Memory: 67,584 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,34 +1,28 @@
+def move(x, y, cnt, s):
+    global max_count, start
+
+    if cnt > max_count or (cnt == max_count and s < start):
+        max_count = cnt
+        start = s
+
+
+    for dx, dy in delta:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < N and nums[x][y] + 1 == nums[nx][ny]:
+            move(nx, ny, cnt + 1, s)
 
 T = int(input())
-dirs = [(1,0), (-1,0), (0,1), (0,-1)]
 
-def dfs(x, y):
-    if dp[x][y] != 0:
-        return dp[x][y]
-
-    dp[x][y] = 1  # 최소 자기 자신 포함
-    for dx, dy in dirs:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N:
-            if rooms[nx][ny] == rooms[x][y] + 1:
-                dp[x][y] = max(dp[x][y], 1 + dfs(nx, ny))
-    return dp[x][y]
-
-for t in range(1, T+1):
+for t in range(1, T + 1):
     N = int(input())
-    rooms = [list(map(int, input().split())) for _ in range(N)]
-    dp = [[0]*N for _ in range(N)]
+    nums = [list(map(int, input().split())) for _ in range(N)]
+    delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-    max_len = 0
-    start_num = N*N + 1
+    max_count = 0
+    start = float('inf')
 
     for i in range(N):
         for j in range(N):
-            length = dfs(i, j)
-            if length > max_len:
-                max_len = length
-                start_num = rooms[i][j]
-            elif length == max_len and rooms[i][j] < start_num:
-                start_num = rooms[i][j]
+            move(i, j, 1, nums[i][j])
 
-    print(f"#{t} {start_num} {max_len}")
+    print(f"#{t} {start} {max_count}")
