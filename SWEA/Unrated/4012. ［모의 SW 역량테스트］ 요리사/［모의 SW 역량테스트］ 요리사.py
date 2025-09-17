2c257a5083cf2bd53135b9a2a207fa597ea38581
Ssongnya <alsrud9972@naver.com>
2025-09-17 14:38:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 503 ms, Memory: 71,808 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.py
@@ -1,43 +1,43 @@
-def synergy(lst):
-    total = 0
 
-    for x in range(len(lst)):
-        for y in range(x + 1, len(lst)):
-            i = lst[x]
-            j = lst[y]
-            total += ingredient[i][j] + ingredient[j][i]
-    
+from collections import deque
+
+def synergy(my_list):
+    total = 0
+    for i in range(len(my_list)):
+        for j in range(i+1,len(my_list)):
+            #이렇게 꺼내면 중복 없이 꺼내진다.
+            ing1 = my_list[i]
+            ing2 = my_list[j]
+            total += ingredient[ing1][ing2] + ingredient[ing2][ing1]
     return total
 
-
-def subset(idx, cnt, chosen):
+def combination(indx,chosen): #인덱스로 해서 절반을 골랐으면 끝내기
     global min_diff
 
-    if cnt > N // 2 :
-        return
-    
-    if idx >= N:
-        if cnt == (N // 2) and len(chosen) == N//2:
-            other = [i for i in range(N) if i not in chosen]
-            s1 = synergy(chosen)
-            s2 = synergy(other)
-            diff = abs(s1 - s2)
-            min_diff = min(diff, min_diff)
+    if len(chosen) == (N // 2):
+        other = [i for i in range(N) if i not in chosen]
+        A = synergy(chosen)
+        B = synergy(other)
+        result = abs(A-B)
+        min_diff = min(min_diff, result)
+
         return
 
-    subset(idx + 1, cnt + 1, chosen + [idx])
-    subset(idx + 1, cnt, chosen)
+    #계속 더하기만 하면 인덱스 에러
+    if indx >= N:
+        return
+    #포함 하냐 안하냐 재귀돌리기!!
+    combination(indx+1,chosen + [indx])
+    combination(indx+1,chosen)
 
 
 T = int(input())
-
-for t in range(1, T + 1):
-    N = int(input())
-    
-    ingredient = [list(map(int, input().split())) for _ in range(N)]
-
+for tc in range(1, T + 1):
+    N = int(input()) # 음식의재료 수 = 행렬의 크기
+    ingredient = [list(map(int,input().split())) for _ in range(N)]
     min_diff = float('inf')
+    combination(0,[])
+    print(f'#{tc} {min_diff}')
+    
 
-    subset(0, 0, [])
 
-    print(f"#{t} {min_diff}")
