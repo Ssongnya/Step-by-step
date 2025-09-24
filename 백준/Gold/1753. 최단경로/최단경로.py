5d88dcbb91c5f3ff7a2c74c1e2b6588c85d88795
Ssongnya <alsrud9972@naver.com>
2025-09-25 01:03:03 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 69516 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+K = int(input())
+
+graph = [[] for _ in range(V + 1)]   # 인접 리스트
+dist = [float('inf')] * (V + 1)      # 최단 거리 배열
+
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+def dijkstra(start):
+    q = []
+    dist[start] = 0
+    heapq.heappush(q, (0, start))
+
+    while q:
+        cost, now = heapq.heappop(q)
+
+        if dist[now] < cost:
+            continue
+
+        for nxt, w in graph[now]:
+            new_cost = cost + w
+            if new_cost < dist[nxt]:
+                dist[nxt] = new_cost
+                heapq.heappush(q, (new_cost, nxt))
+
+dijkstra(K)
+
+for i in range(1, V + 1):
+    print("INF" if dist[i] == float('inf') else dist[i])
