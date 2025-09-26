c9b5f77e0ec1c79b8ef4dd8391539b9094d79f43
Ssongnya <alsrud9972@naver.com>
2025-09-26 19:17:06 +0900

    [D2] Title: 파리 퇴치, Time: 66 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,22 +1,21 @@
+def catch(x, y, arr):
+    n = len(arr)
+    hap = 0
+    for i in range(x, x + M):
+        for j in range(y, y + M):
+            hap += arr[i][j]
+    return hap
+    
+
 T = int(input())
 
 for t in range(1, T + 1):
     N, M = map(int, input().split())
-    grid = [[int(x) for x in input().split()] for _ in range(N)]
-    hap = []
-
+    flies = [list(map(int, input().split())) for _ in range(N)]
+    max_flies = float('-inf')
 
     for i in range(N - M + 1):
         for j in range(N - M + 1):
-            sum_nums = 0
-            for x in range(M):
-                for y in range(M):
-                    sum_nums += grid[i + x][j + y]
-            hap.append(sum_nums)
+            max_flies = max(catch(i, j, flies), max_flies)
 
-    max_hap = hap[0]
-    for num in hap :
-        if num > max_hap :
-            max_hap = num
-
-    print(f"#{t} {max_hap}")
+    print(f"#{t} {max_flies}")
