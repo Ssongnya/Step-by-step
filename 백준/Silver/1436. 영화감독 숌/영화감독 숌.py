85a03e58d5182a90a9f1108889d24c35b4667971
Ssongnya <alsrud9972@naver.com>
2025-11-05 12:33:09 +0900

    [Silver V] Title: 영화감독 숌, Time: 672 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+nums = []
+
+num = 666
+count = 0
+
+while True:
+    if '666' in str(num):
+        count += 1
+
+    if count == N:
+        print(num)
+        break
+
+    num += 1
