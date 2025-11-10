1af0b7bbd4cfe8ea27a37ab661be56f5aedbd279
Ssongnya <alsrud9972@naver.com>
2025-11-10 15:33:32 +0900

    [Gold III] Title: 내리막 길, Time: 624 ms, Memory: 66064 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * M for _ in range(N)]
+
+delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+dp[N-1][M-1] = 1
+
+rooms = [(graph[x][y], x, y) for x in range(N) for y in range(M)]
+rooms.sort()
+
+for _, x, y in rooms:
+    for dx, dy in delta:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] < graph[x][y]:
+            dp[x][y] += dp[nx][ny]
+    
+print(dp[0][0])
