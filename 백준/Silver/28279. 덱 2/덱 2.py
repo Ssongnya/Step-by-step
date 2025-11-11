0e5254bf9fcec5695828d2b1964b66027b41f5a3
Ssongnya <alsrud9972@naver.com>
2025-11-11 21:33:05 +0900

    [Silver IV] Title: 덱 2, Time: 928 ms, Memory: 98760 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+queue = deque()
+result = []
+
+for _ in range(N):
+    order = input().strip().split()
+    if len(order) > 1:
+        if order[0] == '1':
+            queue.appendleft(int(order[-1]))
+            continue
+        else:
+            queue.append(int(order[-1]))
+            continue
+
+    x = int(order[0])
+
+    if x == 3:
+        if queue:
+            result.append(str(queue.popleft()))
+        else:
+            result.append('-1')
+    elif x == 4:
+        if queue:
+            result.append(str(queue.pop()))
+        else:
+            result.append('-1')
+    elif x == 5:
+        result.append(str(len(queue)))
+    elif x == 6:
+        result.append('0' if queue else '1')
+    elif x == 7:
+        result.append(str(queue[0]) if queue else '-1')
+    elif x == 8:
+        result.append(str(queue[-1]) if queue else '-1')
+
+print('\n'.join(result))
