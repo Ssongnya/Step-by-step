fbb01490c77a76069f3f2d4b0049955003f6b688
Ssongnya <alsrud9972@naver.com>
2025-12-02 00:18:52 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 4일차 - 병합 정렬, Time: 1,531 ms, Memory: 188,192 KB -BaekjoonHub

--- SWEA/D3/5204. ［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬.py
+++ SWEA/D3/5204. ［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬/［파이썬 S／W 문제해결 구현］ 4일차 － 병합 정렬.py
@@ -1,43 +1,41 @@
-def merge_sort(lst):
-    global case_num
-    if len(lst) <= 1:
-        return lst
+def merge_sort(arr):
+    global count
     
-    mid = len(lst) // 2
+    n = len(arr)
+    if n <= 1:
+        return arr
 
-    left = merge_sort(lst[:mid])
-    right = merge_sort(lst[mid:])
+    mid = n // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
 
     if left[-1] > right[-1]:
-        case_num += 1
+        count += 1
 
-    return merge(left, right)
-
-
-def merge(left, right):
-    result = []
-    i, j = 0, 0
+    merged = []
+    i = j = 0
 
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
-            result.append(left[i])
+            merged.append(left[i])
             i += 1
         else:
-            result.append(right[j])
+            merged.append(right[j])
             j += 1
 
-    result.extend(left[i:])
-    result.extend(right[j:])
+    merged.extend(left[i:])
+    merged.extend(right[j:])
 
-    return result
+    return merged
+
 
 T = int(input())
-
 for t in range(1, T + 1):
     N = int(input())
-    nums = list(map(int, input().split()))
-    case_num = 0
+    arr = list(map(int, input().split()))
+    
+    count = 0
+    sorted_arr = merge_sort(arr)
 
-    fix_nums = merge_sort(nums)
-
-    print(f"#{t} {fix_nums[N//2]} {case_num}")
+    print(f"#{t} {sorted_arr[N//2]} {count}")
