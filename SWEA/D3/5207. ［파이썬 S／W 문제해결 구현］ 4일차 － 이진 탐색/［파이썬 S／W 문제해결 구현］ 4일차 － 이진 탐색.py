a8400c60bec0101c946a72a26ec1f8b2adc0b669
Ssongnya <alsrud9972@naver.com>
2025-12-09 17:06:10 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 4일차 - 이진 탐색, Time: 124 ms, Memory: 70,784 KB -BaekjoonHub

--- SWEA/D3/5207. ［파이썬 S／W 문제해결 구현］ 4일차 － 이진 탐색/［파이썬 S／W 문제해결 구현］ 4일차 － 이진 탐색.py
+++ SWEA/D3/5207. ［파이썬 S／W 문제해결 구현］ 4일차 － 이진 탐색/［파이썬 S／W 문제해결 구현］ 4일차 － 이진 탐색.py
@@ -1,17 +1,21 @@
 def binary_search(lst, num):
     left, right = 0 , len(lst) - 1
-    direct = 0
+    direct = 0 # 탐색 방향이 -1 이면 왼쪽 봤던거고 1이면 오른쪽 봤던것임
 
     while left <= right:
         mid = (left + right) // 2
         if lst[mid] == num:
             return True
+        
+        # 왼쪽
         elif lst[mid] > num:
             curr_dir = -1
-            if direct == curr_dir :
+            if direct == curr_dir : # 같은 방향을 반복해서 본다면 중단하기
                 return False
             right = mid - 1
             direct = curr_dir
+        
+        # 오른쪽
         else:
             curr_dir = 1
             if direct == curr_dir:
@@ -19,8 +23,7 @@ def binary_search(lst, num):
             left = mid + 1
             direct = curr_dir
     
-    return False
-
+    return False # 이진탐색을 진행했는데도 값ㄷ을찾지 못하면 False반환
 
 T = int(input())
 
